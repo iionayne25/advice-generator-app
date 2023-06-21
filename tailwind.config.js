afae596fac1e236866e6e229a544b98c846b8fa6
iioayne25 <pavinee_sut@cmu.ac.th>
2023-06-21 23:17:08 +0700

    dev in  mobile view

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,15 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+     
     },
+    colors:{
+      "lightCyan": "hsl(193, 38%, 86%)",
+      "neonGreen": "hsl(150, 100%, 66%)",
+ "grayishBlue":"hsl(217, 19%, 38%)",
+ "darkGrayishBlue": "hsl(217, 19%, 24%)",
+ "darkBlue": "hsl(218, 23%, 16%)",
+    }
   },
   plugins: [],
 }
